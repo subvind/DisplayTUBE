20beb5e75d15e586eda4666b02abf28dc526df59
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-11-15 01:21:48 -0600

    auth

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { AuthAccountLogin } from "subvind-components"
+
+	let organization: any;
+
+  onMount(async () => {
+    let tubeHostname = window.location.hostname
+    if (tubeHostname === 'localhost') {
+      tubeHostname = 'videos.subvind.com'
+    }
+    const response = await fetch(`https://api.subvind.com/organizations/tubeHostname/${tubeHostname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      organization = await response.json();
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  })
+</script>
+
+<br />
+<br />
+<AuthAccountLogin organization={organization} />
+
+<style>
+  :global(body) {
+    background: #eee !important;
+  }
+</style>
---
+++ src/routes/auth/logout/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    localStorage.setItem('access_token', '')
+    window.location.href = '/'
+  })
+</script>
